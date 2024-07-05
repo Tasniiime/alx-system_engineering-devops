9370091c183f7eacb92dfe504bac3c1c5e84efa6
root <root@tasnime>
2024-07-05 23:50:18 +0100

    commit

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+# Create /tmp/school file with specific permissions and content
+file { '/tmp/school':
+  ensure  => file,
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
